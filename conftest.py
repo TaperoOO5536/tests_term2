0ceb6200354cbe29a16d653f4c6a7fa399036fee
Olesya <tapero.olesya@yandex.ru>
2025-06-01 15:11:30 +0300

    add conftest

---
+++ conftest.py
@@ -0,0 +1,40 @@
+import pytest
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", default="chrome", choices=["chrome", "firefox"])
+    parser.addoption("--headless", action="store_true")
+    parser.addoption("--executor", action="store", default="127.0.0.1:4444")
+    parser.addoption("--log_level", action="store", default="INFO")
+
+@pytest.fixture(scope="module")
+def driver(request):
+    browser_name = request.config.getoption("--browser")
+
+    if browser_name == "chrome":
+        driver = webdriver.Chrome()
+    elif browser_name == "firefox":
+        driver = webdriver.Firefox()
+    else:
+        raise NotImplementedError(f"Browser {browser_name} is not implemented.")
+    driver.get("http://localhost:8080")
+    yield driver
+    driver.quit()
+
+@pytest.fixture
+def pages(driver):
+    from page_object.Home_page import HomePage
+    from page_object.Product_page import ProductPage
+    from page_object.Cart_page import CartPage
+    from page_object.Login_page import LoginPage
+
+    home_page = HomePage(driver)
+    product_page = ProductPage(driver)
+    cart_page = CartPage(driver)
+    login_page = LoginPage(driver)
+    return home_page, product_page, cart_page, login_page
+
+@pytest.fixture(autouse=True)
+def setup(pages):
+    home_page, _, _, _ = pages
+    home_page.navigate_to("http://localhost:8080")
--- test/cp3_test.py
+++ test/cp3_test.py
@@ -1,73 +1,44 @@
-import pytest
-from selenium import webdriver
-from page_object.Home_page import HomePage
-from page_object.Product_page import ProductPage
-from page_object.Cart_page import CartPage
-from page_object.Login_page import LoginPage
 from selenium.webdriver.common.by import By
-from time import sleep
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-@pytest.fixture(scope="module")
-def driver():
-    driver = webdriver.Chrome()
-    driver.get("http://localhost:8080")
-    yield driver
-    driver.quit()
+def test_add_to_wish_list(pages):
+    home_page, _, _, login_page = pages
+    # login_page.go_to_signup_page()
+    # login_page.signup("Test", "User", "asd@asd.ru", "qweasz1234")
+    login_page.go_to_login_page()
+    login_page.login("asd@asd.ru", "qweasz1234")
+    home_page.go_to_home()
+    home_page.add_to_wish_list_from_home("MacBook", 400)
+    home_page.close_alert()
+    home_page.go_to_wish_list()
+    assert len(home_page.driver.find_elements(By.XPATH, "//td//a[contains(text(), 'MacBook')]")) != 0, "MacBook was not added to the wish list."
 
-@pytest.fixture
-def pages(driver):
-  home_page = HomePage(driver)
-  product_page = ProductPage(driver)
-  cart_page = CartPage(driver)
-  login_page = LoginPage(driver)
-  return home_page, product_page, cart_page, login_page
-  
-pytest.fixture(autouse=True)
-def setup(pages):
-    home_page, _, _, _ = pages
-    home_page.go_to_home()    
+def test_add_camera_to_cart(pages):
+    home_page, product_page, cart_page, _ = pages
+    home_page.go_to_home()
+    product_page.go_to_product_page("Canon EOS 5D", 700)
+    product_page.choose_color("Red")
+    product_page.add_to_cart()
+    product_page.close_alert()
+    home_page.go_to_cart()
+    assert len(home_page.driver.find_elements(By.XPATH, "//td//a[contains(text(), 'Canon EOS 5D')]")) != 0, "Canon EOS 5D was not added to the cart."
 
-# def test_add_to_wish_list(pages):
-#     home_page, _, _, login_page = pages
-#     # login_page.go_to_signup_page()
-#     # login_page.signup("Test", "User", "asd@asd.ru", "qweasz1234")
-#     login_page.go_to_login_page()
-#     login_page.login("asd@asd.ru", "qweasz1234")
-#     home_page.go_to_home()
-#     home_page.add_to_wish_list_from_home("MacBook", 400)
-#     home_page.close_alert()
-#     home_page.go_to_wish_list()
-#     assert len(home_page.driver.find_elements(By.XPATH, "//td//a[contains(text(), 'MacBook')]")) != 0, "MacBook was not added to the wish list."
+def test_add_tablet_to_cart(pages):
+    home_page, product_page, _, _ = pages
+    home_page.go_to_category("tablet")
+    product_page.go_to_product_page("Samsung Galaxy Tab 10.1")
+    product_page.add_to_cart()
+    product_page.close_alert()
+    home_page.go_to_cart()
+    assert len(home_page.driver.find_elements(By.XPATH, "//td//a[contains(text(), 'Samsung Galaxy Tab 10.1')]")) != 0, "Samsung Galaxy Tab 10.1 was not added to the cart."
 
-# def test_add_camera_to_cart(pages):
-#     home_page, product_page, cart_page, _ = pages
-#     home_page.go_to_home()
-#     product_page.go_to_product_page("Canon EOS 5D", 700)
-#     product_page.choose_color("Red")
-#     product_page.add_to_cart()
-#     product_page.close_alert()
-#     home_page.go_to_cart()
-#     assert len(home_page.driver.find_elements(By.XPATH, "//td//a[contains(text(), 'Canon EOS 5D')]")) != 0, "Canon EOS 5D was not added to the cart."
-
-# def test_add_tablet_to_cart(pages):
-#     home_page, product_page, _, _ = pages
-#     home_page.go_to_category("tablet")
-#     product_page.go_to_product_page("Samsung Galaxy Tab 10.1")
-#     product_page.add_to_cart()
-#     product_page.close_alert()
-#     home_page.go_to_cart()
-#     assert len(home_page.driver.find_elements(By.XPATH, "//td//a[contains(text(), 'Samsung Galaxy Tab 10.1')]")) != 0, "Samsung Galaxy Tab 10.1 was not added to the cart."
-
-# def test_add_htc_phone_to_cart(pages):
-#     home_page, product_page, _, _ = pages
-#     home_page.go_to_category("smartphone")
-#     product_page.go_to_product_page("HTC Touch HD")
-#     product_page.add_to_cart()
-#     product_page.close_alert()
-#     home_page.go_to_cart()
-#     assert len(home_page.driver.find_elements(By.XPATH, "//td//a[contains(text(), 'HTC Touch HD')]")) != 0, "HTC Touch HD was not added to the cart."
+def test_add_htc_phone_to_cart(pages):
+    home_page, product_page, _, _ = pages
+    home_page.go_to_category("smartphone")
+    product_page.go_to_product_page("HTC Touch HD")
+    product_page.add_to_cart()
+    product_page.close_alert()
+    home_page.go_to_cart()
+    assert len(home_page.driver.find_elements(By.XPATH, "//td//a[contains(text(), 'HTC Touch HD')]")) != 0, "HTC Touch HD was not added to the cart."
 
 def test_write_review(pages):
     home_page, product_page, _, _ = pages
